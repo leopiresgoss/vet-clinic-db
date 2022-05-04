21479591c2d02cf8ec0ea2408112f4471efc9baf
ForHemer <foreste.hemerson@gmail.com>
2022-05-03 21:59:41 -0400

    Create patients table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,9 @@
+/* Create clinic database */
 CREATE DATABASE clinic;
+
+/* Create patients table */
+CREATE TABLE "patients"(
+    "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "name" VARCHAR(255) NOT NULL,
+    "date_of_birth" DATE NOT NULL
+);
