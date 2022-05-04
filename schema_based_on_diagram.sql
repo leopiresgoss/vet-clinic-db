f6d26e8b1cda33e81eaed47c92b17badd30ce2f0
ForHemer <foreste.hemerson@gmail.com>
2022-05-03 22:00:38 -0400

    Create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,3 +7,11 @@ CREATE TABLE "patients"(
     "name" VARCHAR(255) NOT NULL,
     "date_of_birth" DATE NOT NULL
 );
+
+CREATE TABLE "medical_histories"(
+    "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "admitted_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "patient_id" INTEGER NOT NULL,
+    "status" VARCHAR(255) NOT NULL,
+    FOREIGN KEY("patient_id") REFERENCES "patients"("id")
+);
